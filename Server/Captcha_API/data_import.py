fef24ff7aa1833d0e4a65113d58a66778da41d7b
HXY <63409863+Cassie-cpu@users.noreply.github.com>
2020-06-20 14:16:37 +0800

    Update data_import.py

--- Server/Captcha_API/data_import.py
+++ Server/Captcha_API/data_import.py
@@ -4,6 +4,6 @@ from pymongo import MongoClient
 client = MongoClient()
 with open('newcaptcha_img_list.json','r',encoding='utf-8')as f:
     img_url = json.loads(f.read())
-img_url_collection = client['test']['img']
+img_url_collection = client['test']['test']
 img_url_collection.insert_many(img_url)
 print('数据导入成功!')
