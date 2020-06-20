a26e63cba2f62fc5b5060ef0e6bf8b79259fa91a
杜乐然 <65540762+ghostspark@users.noreply.github.com>
2020-06-20 09:23:48 +0800

    Update pt_com.py

--- Client/51JobCompanyInfo/pt_com.py
+++ Client/51JobCompanyInfo/pt_com.py
@@ -9,7 +9,6 @@ with open('C:\DLR\dlr-workplace\项目\数据样式(1) - 副本.json','r+',encod
 
 #制表    
     tb.field_names = ["name", "attribute", "city", "trade","id"]
-    print(tb)
     for i in range(len(data)):
         tb.add_row([data[i]["com_name"],data[i]["com_attribute"],data[i]["com_city"],data[i]["com_trade"],i+1])
     print(tb)    
