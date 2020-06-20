35c653f845fb7892599de94c1285213cf2196800
Yokey <zyf6@outlook.com>
2020-06-21 07:40:06 +0800

    修正路径

--- Server/Captcha_API/data_process.py
+++ Server/Captcha_API/data_process.py
@@ -1,5 +1,5 @@
 import json
-with open('Data/captcha_img_list.json','r',encoding='utf-8')as f:
+with open('Server/Data/captcha_img_list.json','r',encoding='utf-8')as f:
     captcha_img_list = json.loads(f.read())
 
 newcaptcha_img_list = []
@@ -10,6 +10,6 @@ for j in captcha_img_list:
 for i in range(len(captcha_img_list)):
     newcaptcha_img_list.append({"img":captcha_img_list[i]})
 
-with open('Data/newcaptcha_img_list.json','w',encoding='utf-8')as f:
+with open('Server/Data/newcaptcha_img_list.json','w',encoding='utf-8')as f:
     f.write(json.dumps(newcaptcha_img_list))
 print('数据整理完成')
