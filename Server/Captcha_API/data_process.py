15bd19f3e920e49497ca8c2d1f2361901bbd06c5
HXY <63409863+Cassie-cpu@users.noreply.github.com>
2020-06-20 22:26:21 +0800

    Update data_process.py

--- Server/Captcha_API/data_process.py
+++ Server/Captcha_API/data_process.py
@@ -1,12 +1,15 @@
 import json
-with open('C:\data\captcha_img_list.json','r',encoding='utf-8')as f:
+with open('D:\线上工作室\项目孵化\web\data\captcha_img_list.json','r',encoding='utf-8')as f:
     captcha_img_list = json.loads(f.read())
 
 newcaptcha_img_list = []
 
+for j in captcha_img_list:
+    j.replace('\n','')
+    
 for i in range(len(captcha_img_list)):
     newcaptcha_img_list.append({"img":captcha_img_list[i]})
-
-with open('newcaptcha_img_list.json','w',encoding='utf-8')as f:
+    
+with open('Server/Data/newcaptcha_img_list.json','w',encoding='utf-8')as f:
     f.write(json.dumps(newcaptcha_img_list))
 print('数据整理完成')
