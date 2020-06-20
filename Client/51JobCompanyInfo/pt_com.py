f462466db736d7df2c2bd591171113e1f9a2fb9a
杜乐然 <65540762+ghostspark@users.noreply.github.com>
2020-06-20 09:20:38 +0800

    Update pt_com.py

--- Client/51JobCompanyInfo/pt_com.py
+++ Client/51JobCompanyInfo/pt_com.py
@@ -1,9 +1,10 @@
 from prettytable import PrettyTable
 import json
 #读取数据
+#文件为绝对路径。
 with open('C:\DLR\dlr-workplace\项目\数据样式(1) - 副本.json','r+',encoding='utf-8') as ifo:
     ls=ifo.read()
-    data=json.loads(ls)
+    data=json.loads(ls)#注意json文件中1.'改为";2.文件中字典间的逗号。
     tb=PrettyTable()
 
 #制表    
