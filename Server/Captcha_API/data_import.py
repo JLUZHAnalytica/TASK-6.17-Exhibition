3fb533c074e164d9da7c3a550c196848a63b4610
Yokey <zyf6@outlook.com>
2020-06-20 18:02:48 +0800

    修正文件路径

--- Server/Captcha_API/data_import.py
+++ Server/Captcha_API/data_import.py
@@ -2,7 +2,7 @@ import json
 from pymongo import MongoClient
 
 client = MongoClient()
-with open('newcaptcha_img_list.json','r',encoding='utf-8')as f:
+with open('Data/newcaptcha_img_list.json','r',encoding='utf-8')as f:
     img_url = json.loads(f.read())
 img_url_collection = client['test']['test']
 img_url_collection.insert_many(img_url)
--- Server/Captcha_API/data_process.py
+++ Server/Captcha_API/data_process.py
@@ -1,5 +1,5 @@
 import json
-with open('C:\data\captcha_img_list.json','r',encoding='utf-8')as f:
+with open('Data/captcha_img_list.json','r',encoding='utf-8')as f:
     captcha_img_list = json.loads(f.read())
 
 newcaptcha_img_list = []
@@ -7,6 +7,6 @@ newcaptcha_img_list = []
 for i in range(len(captcha_img_list)):
     newcaptcha_img_list.append({"img":captcha_img_list[i]})
 
-with open('newcaptcha_img_list.json','w',encoding='utf-8')as f:
+with open('Data/newcaptcha_img_list.json','w',encoding='utf-8')as f:
     f.write(json.dumps(newcaptcha_img_list))
 print('数据整理完成')
