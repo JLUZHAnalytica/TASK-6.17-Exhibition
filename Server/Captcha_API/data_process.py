60540cd56da6f06bb8f6ad67fdf2f1ca2ef10d0e
HXY <63409863+Cassie-cpu@users.noreply.github.com>
2020-06-24 19:15:46 +0800

    已读未回

--- Server/Captcha_API/data_process.py
+++ Server/Captcha_API/data_process.py
@@ -8,8 +8,8 @@ for j in captcha_img_list:
     j.replace('\n','')
     
 for i in range(len(captcha_img_list)):
-    newcaptcha_img_list.append({"img":captcha_img_list[i]})
-
+    newcaptcha_img_list.append({"id":i,"title":"","img":captcha_img_list[i]})
+    
 with open('Server/Data/newcaptcha_img_list.json','w',encoding='utf-8')as f:
     f.write(json.dumps(newcaptcha_img_list))
 print('数据整理完成')
